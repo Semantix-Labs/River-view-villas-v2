5f614b3cec4a7fe6d3eccd550850aff768af93a3
shobian <s.shobian@gmail.com>
2026-05-17 10:32:03 +0530

    GTM intergated

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <Script id="gtm" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P5V6J6QX');`}
+        </Script>
         <Script id="meta-pixel" strategy="afterInteractive">
           {`
             !function(f,b,e,v,n,t,s)
@@ -40,6 +47,13 @@ export default function RootLayout({
         </Script>
       </head>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P5V6J6QX"
+            height="0" width="0"
+            style={{display:'none', visibility:'hidden'}}
+          />
+        </noscript>
         <noscript>
           <img height="1" width="1" style={{display:"none"}}
             src="https://www.facebook.com/tr?id=26739144672414876&ev=PageView&noscript=1"
